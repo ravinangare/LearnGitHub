3f9f5388bd67e7df1ef33a3909fc5f37dfbb6497
ravinangare <ravinangre@gmail.com>
2026-09-07 07:32:23 +0530

    New Login Test

---
+++ tests/firstTest.spec.js
@@ -0,0 +1,8 @@
+import{test,expect} from '@playwright/test'
+
+test('First Test',async({page})=>{
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    await page.getByPlaceholder("Username").fill("admin")
+    await page.getByPlaceholder("Password").fill("admin123")
+    await page.getByRole('button', { name: 'Login' }).click()
+})
